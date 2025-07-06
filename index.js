d553f99f347a94ae1ff70dabdcdc4741c9db79f5
hamodi0-0 <mh9988527@gmail.com>
2025-07-06 06:51:02 +0300

    fetching book cover image

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import pg from 'pg';
 import bodyParser from 'body-parser';
+import axios from 'axios';
  
 
 const app = express();
@@ -24,6 +25,28 @@ app.get("/", (req, res)=>{
     res.render("index.ejs");
 })
 
+app.post("/search", async (req, res)=>{
+    const searchText = req.body.search;
+
+    // formatting the request to create an appropriate link 
+    const searchQuery = searchText.toLowerCase().split(' ').join('+');
+    console.log(searchQuery);
+
+    // fetching book data
+    const result = await axios.get(`https://openlibrary.org/search.json?q=${searchQuery}`);
+
+    // console.log(result.data.docs[0])
+
+    // generating image url
+    const imageId = result.data.docs[0].cover_i
+    const imageUrl = `https://covers.openlibrary.org/b/id/${imageId}-L.jpg`
+    // console.log(imageUrl)
+
+    res.render("index.ejs", {
+        data : true,
+        imageUrl,
+    });
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server listening on port ${PORT}...`)
